5681bc922bd99a72d909337756231603465dfa6e
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-02-11 21:01:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -16,12 +16,8 @@ end
 
 introduction_with_language_optional("Edwin","Ruby")
 
-def introduction_with_language_optional(name,language)
-  language = "Ruby"
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
+introduction_with_language_optional("Chris", language = "Ruby")
 
-introduction_with_language("Chris",language)
 
 
 
